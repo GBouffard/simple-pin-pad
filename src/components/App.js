f8e12a1633af62f286b13370cbfcea1ca5d5258b
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-18 10:29:12 +0000

    Add quick wireframe of pin pad representation

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,46 @@ class App extends Component {
 
         <header
           className="App-header">
-
-          <h1
-            className="App-title">
-            Guillaume's Simple Pin Pad
-          </h1>
-
+          Guillaume's Simple Pin Pad
         </header>
 
+      <div>
+        <div>
+          Screen display
+        </div>
+        <section>
+          <button>
+            1
+          </button>
+          <button>
+            2
+          </button>
+          <button>
+            3
+          </button>
+          <button>
+            4
+          </button>
+          <button>
+            5
+          </button>
+          <button>
+            6
+          </button>
+          <button>
+            7
+          </button>
+          <button>
+            8
+          </button>
+          <button>
+            9
+          </button>
+          <button>
+            0
+          </button>
+        </section>
+      </div>
       </div>
     );
   }
