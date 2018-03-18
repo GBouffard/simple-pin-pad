ffa82a496c62de0e18c9e517fb3cc473cf775095
Guillaume Bouffard <guillaume@getkahoot.com>
2018-03-18 19:54:13 +0000

    Add classnames to node package

--- src/components/atoms/button.js
+++ src/components/atoms/button.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Button = ({ text, onClick, className }) => (
-  <button
-    onClick={onClick}
-    className={className}>
-    {text}
-  </button>
-);
+const Button = ({ text, onClick, className }) => {
+  const buttonClassName = classNames('App__pin-pad-button', className);
+
+  return (
+    <button
+      onClick={onClick}
+      className={buttonClassName}>
+      {text}
+    </button>
+  );
+};
 
 export default Button;
 
